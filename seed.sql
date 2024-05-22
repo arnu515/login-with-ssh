098698c85f24804a3c3f98fcebf7db8d72ba358b
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-05-22 14:38:24 +0530

    feat: add new table, make username and email fields unique

--- seed.sql
+++ seed.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE users (
     id TEXT PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE ssh_public_keys (
@@ -11,4 +11,12 @@ CREATE TABLE ssh_public_keys (
     user_id TEXT NOT NULL,
     key TEXT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE ssh_queue (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    key_id TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(key_id) REFERENCES ssh_public_keys(id)
 );
